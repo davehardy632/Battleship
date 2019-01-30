a854fedd026edd26f653f8b67d2794b115be5d79
David Hardy <davehardy632@gmail.com>
2019-01-30 15:43:31 -0700

    Finished.render method in first iteration

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,19 +1,21 @@
 require 'pry'
 
 class Cell
-  attr_reader :coordinate, :ship
+  attr_reader :coordinate, :ship, :fired_upon, :empty
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
     @fired_upon = false
+    @empty = true
   end
 
   def empty?
-    @ship.nil?
+    @empty
   end
 
   def place_ship(ship)
     @ship = ship
+    @empty = false
   end
 
   def fired_upon?
@@ -22,15 +24,23 @@ class Cell
 
 
   def fire_upon
-    if empty? == false
-      @ship.health -= 1
-      @fired_upon = true
-    else
+    if @empty == false
+      @ship.hit
     end
+    @fired_upon = true
   end
 
-
-
-
-
+  def render
+    if @fired_upon == false
+      p "."
+    elsif
+      @fired_upon == true && empty? == true
+      p "M"
+    elsif
+      @fired_upon == true && empty? == false && @ship.sunk? == false
+      p "H"
+    elsif @fired_upon = true && @ship.sunk? ==  true
+      p "X"
+    end
+  end
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -48,34 +48,81 @@ class CellTest < Minitest::Test
      cell = Cell.new("B4")
      cruiser = Ship.new("Cruiser", 3)
      cell.place_ship(cruiser)
-     # cell.ship
+     cell.ship
+     assert_equal cruiser, cell.ship
      assert_equal false, cell.empty?
    end
 
    def test_that_cell_is_fired_upon?
+
      cell = Cell.new("B4")
 
      assert_equal false, cell.fired_upon?
    end
 
    def test_that_ship_has_been_fired_upon
+
      cell = Cell.new("B4")
      cruiser = Ship.new("Cruiser", 3)
      cell.place_ship(cruiser)
      cell.fire_upon
      cell.fire_upon
 
-     assert_equal 1, cruiser.health
+     assert_equal 1, cell.ship.health
    end
 
-   def test_that_being_fired_upon_returns_boolean
+   def test_if_ship_is_fired_upon
+
      cell = Cell.new("B4")
      cruiser = Ship.new("Cruiser", 3)
      cell.place_ship(cruiser)
      cell.fire_upon
-    
+
      assert_equal true, cell.fired_upon?
    end
 
+   def test_if_cell_1_is_a_cell
 
+     cell_1 = Cell.new("B4")
+     cruiser = Ship.new("Cruiser", 3)
+
+     assert_instance_of Cell, cell_1
+   end
+
+   def test_if_cell_1_renders
+
+     cell_1 = Cell.new("B4")
+     cruiser = Ship.new("Cruiser", 3)
+
+     cell_1.fire_upon
+     assert_equal "M", cell_1.render
+   end
+
+   def test_if_cell_renders_H
+     cell_1 = Cell.new("B4")
+     cruiser = Ship.new("Cruiser", 3)
+     cell_1.place_ship(cruiser)
+     cell_1.fire_upon
+
+     assert_equal "H", cell_1.render
+   end
+
+   def test_that_cell_has_not_been_fired_upon
+     cell_1 = Cell.new("B4")
+     cruiser = Ship.new("Cruiser", 3)
+     cell_1.place_ship(cruiser)
+     assert_equal ".", cell_1.render
+   end
+
+   def test_if_ship_is_sunk_in_cell
+
+     cell_1 = Cell.new("B4")
+     cruiser = Ship.new("Cruiser", 3)
+     cell_1.place_ship(cruiser)
+     cell_1.fire_upon
+     cell_1.fire_upon
+     cell_1.fire_upon
+
+     assert_equal "X", cell_1.render
+   end
 end
