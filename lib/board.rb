6aca7087d59261b4420e68e5939ce36e23096596
David Hardy <davehardy632@gmail.com>
2019-02-05 16:18:28 -0700

    Completed Iteration 2

---
+++ lib/board.rb
@@ -0,0 +1,93 @@
+require 'pry'
+
+class Board
+  attr_reader :cells, :extract_letters, :extract_numbers
+  def initialize
+    @cells = {
+      "A1" => Cell.new("A1"),
+      "A2" => Cell.new("A2"),
+      "A3" => Cell.new("A3"),
+      "A4" => Cell.new("A4"),
+      "B1" => Cell.new("B1"),
+      "B2" => Cell.new("B2"),
+      "B3" => Cell.new("B3"),
+      "B4" => Cell.new("B4"),
+      "C1" => Cell.new("C4"),
+      "C2" => Cell.new("C2"),
+      "C3" => Cell.new("C3"),
+      "C4" => Cell.new("C4"),
+      "D1" => Cell.new("D1"),
+      "D2" => Cell.new("D2"),
+      "D3" => Cell.new("D3"),
+      "D4" => Cell.new("D4")
+    }
+  end
+
+  def valid_coordinate?(cell)
+    @cells.has_key?(cell)
+  end
+
+  def return_flattened_array(coordinates)
+    coordinates = coordinates.map do |cell| cell.split(//)
+    end
+    coordinates = coordinates.flatten
+  end
+
+  def extract_numbers(coordinates)
+    numbers = coordinates.select do |element| coordinates.index(element).odd?
+    end
+  end
+
+  def extract_letters(coordinates)
+    letters = coordinates.select do |element|
+      coordinates.index(element).even?
+    end
+  end
+
+  def ships_overlap?(ship, coordinates)
+    coordinates.map do |coordinate|
+      @cells[coordinate]
+    end.any? { |cell| cell.occupied? }
+  end
+
+  def valid_placement?(ship, coordinates)
+    if ships_overlap?(ship, coordinates)
+      return false
+    else
+    coordinates = return_flattened_array(coordinates)
+    letter_box = ["A", "B", "C", "D", "E", "F", "G", "H", "I"].join
+    number_box = ["1", "2", "3", "4", "5", "6", "7", "8", "9"].join
+    letters = extract_letters(coordinates).join
+    numbers = extract_numbers(coordinates).join
+    if letters.count(letters[0]) == ship.length
+      if number_box.include?(numbers) == true
+        true
+      else false
+      end
+    elsif numbers.count(numbers[0]) == ship.length
+      if letter_box.include?(letters) == true
+        true
+      else false
+      end
+    else false
+    end
+  end
+end
+
+  def place(ship, coordinates)
+    if valid_placement?(ship, coordinates)
+      true
+      coordinates.each do |cell|
+        @cells[cell].place_ship(ship)
+      end
+    end
+  end
+
+  def render(hidden = false)
+    p "  1 2 3 4 \n" +
+     "A #{@cells["A1"].render(hidden)} #{@cells["A2"].render(hidden)} #{@cells["A3"].render(hidden)} #{@cells["A4"].render(hidden)} \n" +
+     "B #{@cells["B1"].render(hidden)} #{@cells["B2"].render(hidden)} #{@cells["B3"].render(hidden)} #{@cells["B4"].render(hidden)} \n" +
+     "C #{@cells["C1"].render(hidden)} #{@cells["C2"].render(hidden)} #{@cells["C3"].render(hidden)} #{@cells["C4"].render(hidden)} \n" +
+     "D #{@cells["D1"].render(hidden)} #{@cells["D2"].render(hidden)} #{@cells["D3"].render(hidden)} #{@cells["D4"].render(hidden)} \n"
+  end
+end
---
+++ test/board_test.rb
@@ -0,0 +1,122 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require 'pry'
+
+class BoardTest < Minitest::Test
+
+  def test_that_board_exists
+    board = Board.new
+    assert_instance_of Board, board
+  end
+  def test_that_board_has_cells
+    board = Board.new
+    assert_equal 16, board.cells.length
+  end
+
+  def test_that_coordinates_are_valid
+    board = Board.new
+    assert_equal true, board.valid_coordinate?("B3")
+    assert_equal true, board.valid_coordinate?("D1")
+    assert_equal false, board.valid_coordinate?("C18")
+    assert_equal false, board.valid_coordinate?("D5")
+  end
+
+  def test_extract_letters
+    board = Board.new
+
+    assert_equal ["A", "A", "A"], board.extract_letters(["A", "1", "A", "2", "A", "4"])
+  end
+
+  def test_extract_numbers
+
+    board = Board.new
+    assert_equal ["1", "2", "4"], board.extract_numbers(["A","1", "A", "2", "A", "4"])
+  end
+
+  def test_valid_placement
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    assert_equal true, board.valid_placement?(cruiser, ["A1", "A2", "A3"])
+    assert_equal true, board.valid_placement?(cruiser, ["A1", "B1", "C1"])
+    assert_equal false, board.valid_placement?(submarine, ["B2", "C2", "D2"])
+    assert_equal true, board.valid_placement?(submarine, ["A1", "B1"])
+    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2", "A4"])
+    assert_equal false, board.valid_placement?(submarine, ["A1", "C1"])
+    assert_equal false, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
+    assert_equal true, board.valid_placement?(submarine, ["A1", "A2"])
+    assert_equal true, board.valid_placement?(cruiser, ["B1", "C1", "D1"])
+  end
+
+  def test_it_cant_be_diagonal
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    assert_equal false,  board.valid_placement?(cruiser, ["A1", "B2", "C3"])
+    assert_equal false, board.valid_placement?(submarine, ["C2", "D3"])
+  end
+
+  def test_ship_placement
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    cell_1 = board.cells["A1"]
+    cell_2 = board.cells["A2"]
+    cell_3 = board.cells["A3"]
+    cell_4 = board.cells["C1"]
+    cell_5 = board.cells["C2"]
+    board.place(submarine, ["C1", "C2"])
+    assert_equal cruiser, cell_1.ship
+    assert_equal cruiser, cell_2.ship
+    assert_equal cruiser, cell_3.ship
+    assert_equal submarine, cell_4.ship
+    assert_equal submarine, cell_5.ship
+    assert_equal cell_1.ship, cruiser
+    assert_equal true, cell_1.ship == cell_2.ship
+  end
+
+  def test_are_the_ships_overlapping?
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    submarine = Ship.new("Submarine", 2)
+    assert_equal true, board.ships_overlap?(submarine, ["A1", "B1"])
+    assert_equal false, board.ships_overlap?(submarine, ["B1", "C1"])
+  end
+
+  def test_its_still_valid_placement
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    submarine = Ship.new("Submarine", 2)
+    assert_equal false, board.valid_placement?(submarine, ["A1", "B1"])
+    assert_equal true, board.valid_placement?(cruiser, ["C1", "C2", "C3"])
+  end
+
+  def test_that_board_is_rendered
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.render
+    board.place(cruiser, ["A1", "A2", "A3"])
+    expected =  "  1 2 3 4 \n" +
+                "A . . . . \n" +
+                "B . . . . \n" +
+                "C . . . . \n" +
+                "D . . . . \n"
+
+    expected_show_ship =  "  1 2 3 4 \n" +
+                          "A S S S . \n" +
+                          "B . . . . \n" +
+                          "C . . . . \n" +
+                          "D . . . . \n"
+
+    assert_equal expected, board.render
+    assert_equal expected_show_ship, board.render(true)
+  end
+end
